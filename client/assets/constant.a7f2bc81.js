e61077826343fdc28b0a32a90b453e9c7215d636
Cuong Nguyen <cuongnt53@gmail.com>
2015-12-24 15:09:06 +0700

    dad

---
+++ client/assets/constant.a7f2bc81.js
@@ -0,0 +1,230 @@
+var App = {
+  buttons: {
+    start : "Start",
+    subscribe: "Subscribe",
+    buyNow: "Buy Now"
+  },
+  subscribe: {
+    title: "The Beauty Box",
+    image: "http://placehold.it/600x400?text=Image",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis varius sapien sed pretium. Vestibulum pharetra viverra erat tempus consectetur. Phasellus laoreet sagittis libero vel fringilla. Vestibulum bibendum mauris vitae dolor tempor euismod. Curabitur non dictum lorem, ac pharetra velit. Pellentesque ut volutpat risus.",
+    tweets: {
+      title: "Subscribe tweets",
+      items: [
+        {
+          avatar: "http://placehold.it/100x100?text=avt",
+          name: "Name",
+          date: "dd MM",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis varius sapien sed pretium."
+        },
+        {
+          avatar: "http://placehold.it/100x100?text=avt",
+          name: "Name",
+          date: "dd MM",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis varius sapien sed pretium."
+        },
+        {
+          avatar: "http://placehold.it/100x100?text=avt",
+          name: "Name",
+          date: "dd MM",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis varius sapien sed pretium."
+        },
+        {
+          avatar: "http://placehold.it/100x100?text=avt",
+          name: "Name",
+          date: "dd MM",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis varius sapien sed pretium."
+        }
+      ]
+    }
+  },
+
+  quiz : {
+    header: "The Joli Quiz",
+    title: "You are about to join Joli!",
+    firstLine: "Take this fun 3 minute quiz to reveal what you can get!",
+    text: "You can answers here will help us personalize your experience on joli.com and in your Glam Bag."
+  },
+
+  brands: {
+    title: "Our Brands",
+    search: "Search for a brand",
+    logos: [
+      {
+        name : "banker's secret",
+        image: "/assets/images/brands/bankers-secret.5d1760c6.jpg"
+      },
+      {
+        name : "estee lauder.",
+        image: "/assets/images/brands/estee-lauder.1e8af3b0.png"
+      },
+      {
+        name : "innissfree",
+        image: "/assets/images/brands/innissfree.6c8bf221.png"
+      },
+      {
+        name : "A",
+        image: "http://placehold.it/300x300?text=logo"
+      },
+      {
+        name : "A",
+        image: "http://placehold.it/300x300?text=logo"
+      },
+      {
+        name : "A",
+        image: "http://placehold.it/300x300?text=logo"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x300?text=logo"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x300?text=logo"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x300?text=logo"
+      },
+      {
+        name : "C",
+        image: "http://placehold.it/300x300?text=logo"
+      },
+      {
+        name : "C",
+        image: "http://placehold.it/300x300?text=logo"
+      },
+      {
+        name : "C",
+        image: "http://placehold.it/300x300?text=logo"
+      }
+    ]
+  },
+
+  videos: {
+    title: "Our Tutorials",
+    search: "Search for a video",
+    items: [
+      {
+        name : "A",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "A",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "A",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "A",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "A",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "A",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "A",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "A",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "A",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "B",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "C",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "C",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "C",
+        image: "http://placehold.it/300x250?text=video"
+      },
+      {
+        name : "C",
+        image: "http://placehold.it/300x250?text=video"
+      }
+    ]
+  }
+};
